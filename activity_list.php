e7a823015e33f6b8072e007a614d1050fcd45bc9
CelineDupont <celinedupont80@gmail.com>
2016-12-20 13:51:18 -0500

    Update activity_list.php

--- activity_list.php
+++ activity_list.php
@@ -6,4 +6,4 @@ $sql = mysqli_query($connection, "SELECT * FROM activity");
 while ($row = $sql->fetch_assoc()){
     echo '<option value="'.$row['id'].'">' . $row['activityname'] . '</option>';
 }
-?>
+
